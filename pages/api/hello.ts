66e8d3e110d31ea6670e2a6452496b16a0b95a26
biruktesfu <it.biruk.tesfaye@gmail.com>
2024-09-23 09:00:48 +0300

    folder structure update

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
-export default function handler(
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@vercel/postgres";
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const client = await db.connect();
+  try {
+    await client.sql`DROP TABLE IF EXISTS testing`;
+    await client.sql`CREATE TABLE IF NOT EXISTS testing (Name varchar(100), Age int)`;
+    const names = [
+      {
+        name: "biruk",
+        age: 24,
+      },
+      {
+        name: "Ermiyas",
+        age: 21,
+      },
+    ];
+
+    names.map(({ name, age }: any) => {
+      client.sql`insert into testing (Name, Age) values (${name}, ${age})`;
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+  const users = await client.sql`select * from testing`;
+  console.log({ users });
+  return res.status(200).json({ users: users.rows });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>this is home</div>;
+  const test = async () => {
+    // let data = await fetch("/api/hello");
+    // let posts = await data.json();
+    // console.log({ posts });
+    axios
+      .get("/api/hello")
+      .then((val) => {
+        console.log("val is => ", val);
+      })
+      .catch((error) => {
+        console.log("error is => ", error);
+      });
+  };
+
+  return (
+    <div>
+      <button onClick={() => test()}>testing</button>
+    </div>
+  );
 }
